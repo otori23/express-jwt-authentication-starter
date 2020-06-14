4fbc7f9296ce9494c0c124143b4fb5abdd141c42
otori23 <azeez.otori@gmail.com>
2020-06-14 12:42:30 -0700

    implement passport jwt strategy

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 const User = require('mongoose').model('User');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-// TODO
-const options = {};
+// Authoriztion header is structured like
+// Authorization: Bearer <jwtToken>
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['RS256'],
+};
 
-// TODO
-module.exports = (passport) => {}
+const strategy = new JwtStrategy(options, (payload, done) => {
+  // at this point the passport framework has already validated the JWT
+  User.findOne({ _id: payload.sub })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => done(err, nullj));
+});
+
+module.exports = (passport) => {
+  passport.use(strategy);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,74 @@
 const mongoose = require('mongoose');
-const router = require('express').Router();   
+const router = require('express').Router();
 const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
 
 // TODO
-router.get('/protected', (req, res, next) => {
+router.get(
+  '/protected',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'You are authorized!' });
+  }
+);
+
+// TODO
+router.post('/login', function (req, res, next) {
+  const { username, password } = req.body;
+
+  User.findOne({ username }).then((user) => {
+    if (!user) {
+      res.status(401).json({
+        success: false,
+        msg: 'could not find user',
+      });
+    }
+
+    const { hash, salt } = user;
+    const isValid = utils.validPassword(password, hash, salt);
+
+    if (isValid) {
+      const { token, expires: expiresIn } = utils.issueJWT(user);
+      res.status(200).json({
+        success: true,
+        user,
+        token,
+        expiresIn,
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        msg: 'you entered the wrong password',
+      });
+    }
+  });
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/register', function (req, res, next) {
+  const { username, password } = req.body;
+  console.log(username, password);
+  const { salt, hash } = utils.genPassword(password);
 
-// TODO
-router.post('/register', function(req, res, next){});
+  const newUser = new User({
+    username,
+    hash,
+    salt,
+  });
+
+  newUser
+    .save()
+    .then((user) => {
+      const { token, expires: expiresIn } = utils.issueJWT(user);
+      res.json({
+        success: true,
+        user,
+        token,
+        expiresIn,
+      });
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
